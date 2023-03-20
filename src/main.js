26a8a69b57da803588998b31db814b82a17595a4
Bryan Pimenta <bryanpimenta30@gmail.com>
2023-03-20 13:40:01 -0300

    Botões com funções implementadas

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import './style.css';
 const dog = document.querySelector('#btn-dog');
 const cat = document.querySelector('#btn-cat');
 const img = document.querySelector('#cute-image');
+const random = document.querySelector('#random');
 
 dog.addEventListener('click', () => {
     fetch('https://dog.ceo/api/breeds/image/random')
@@ -17,5 +18,17 @@ cat.addEventListener('click', () => {
         .then((response) => response.json())
         .then((data) => {
             img.src = data.file;
+        });
+});
+
+random.addEventListener('click', () => {
+    Promise.any([
+        fetch('https://aws.random.cat/meow'),
+        fetch('https://dog.ceo/api/breeds/image/random'),
+    ])
+        .then((res) => res.json())
+        .then((data) => {
+            const petURL = data.file || data.message;
+            img.src = petURL;
         });
 });
