633d39970d287f99abc2f356045342335a541a93
Bryan Pimenta <bryanpimenta30@gmail.com>
2023-03-20 12:34:48 -0300

    Adicionada função de gerar image do doguinho

--- src/main.js
+++ src/main.js
@@ -1,1 +1,13 @@
 import './style.css';
+
+const dog = document.querySelector('#btn-dog');
+/* const cat = document.querySelector('#btn-cat'); */
+const img = document.querySelector('#cute-image');
+
+dog.addEventListener('click', () => {
+    fetch('https://dog.ceo/api/breeds/image/random')
+        .then((response) => response.json())
+        .then((data) => {
+            img.src = data.message;
+        });
+});
